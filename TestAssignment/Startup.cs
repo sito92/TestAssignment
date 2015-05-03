540496ebacc9b1790fbff3a9e50ccbbc65b6942f
Artur Stelmach <arturstelmach92@gmail.com>
2015-05-03 15:16:53 +0200

    Solution created

---
+++ TestAssignment/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestAssignment.Startup))]
+namespace TestAssignment
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
