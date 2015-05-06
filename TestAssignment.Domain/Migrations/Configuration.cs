9821a2b132e34a888c1cd4e55fc8641544255136
Artur Stelmach <arturstelmach92@gmail.com>
2015-05-06 20:01:21 +0200

    Seed method

--- TestAssignment.Domain/Migrations/Configuration.cs
+++ TestAssignment.Domain/Migrations/Configuration.cs
@@ -1,3 +1,5 @@
+using TestAssignment.Domain.Models.DomainModels;
+
 namespace TestAssignment.Domain.Migrations
 {
     using System;
@@ -26,6 +28,90 @@ namespace TestAssignment.Domain.Migrations
             //      new Person { FullName = "Rowan Miller" }
             //    );
             //
+
+
+
+            context.Categories.AddOrUpdate(
+                x=>x.Name,
+                new Category() { Name = "Fruits"},
+                new Category() { Name = "Drinks"},
+                new Category() { Name = "Vegetables"},
+                new Category() { Name = "Meat"}
+                );
+
+            context.Supliers.AddOrUpdate(
+                x=>x.Name,
+                new Suplier() { Name = "Suplier 1"},
+                  new Suplier() { Name = "Suplier 2"},
+                  new Suplier() { Name = "Suplier 3"}
+                );
+
+            context.Products.AddOrUpdate(
+                x => x.ProductName,
+                new Product()
+                {
+                    CategoryId = 1,
+                    DeliverPeriod = 10,
+                    MinStock = "32",
+                    Price = 2,
+                    ProductName = "Apple",
+                    SuplierId = 1
+                },
+                new Product()
+                {
+                    CategoryId = 2,
+                    DeliverPeriod = 2,
+                    MinStock = "54",
+                    Price = 2,
+                    ProductName = "Coca-Cola",
+                    SuplierId = 2
+                },
+                new Product()
+                {
+                    CategoryId = 3,
+                    DeliverPeriod = 21,
+                    MinStock = "10",
+                    Price = 2,
+                    ProductName = "Carrot",
+                    SuplierId = 3
+                },
+                new Product()
+                {
+                    CategoryId = 4,
+                    DeliverPeriod = 32,
+                    MinStock = "54",
+                    Price = 2,
+                    ProductName = "Chicken",
+                    SuplierId = 1
+                },
+                new Product()
+                {
+                    CategoryId = 1,
+                    DeliverPeriod = 12,
+                    MinStock = "23",
+                    Price = 2,
+                    ProductName = "Strawberry",
+                    SuplierId = 2
+                },
+                new Product()
+                {
+                    CategoryId = 2,
+                    DeliverPeriod = 32,
+                    MinStock = "32",
+                    Price = 2,
+                    ProductName = "Pepsi",
+                    SuplierId = 3
+                },
+                new Product()
+                {
+                    CategoryId = 3,
+                    DeliverPeriod = 11,
+                    MinStock = "12",
+                    Price = 2,
+                    ProductName = "Tomato",
+                    SuplierId = 1
+                });
+
         }
     }
 }
